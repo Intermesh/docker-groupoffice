ae67a67c3e013a9f4a1ea4142481d72905b1f2ae
Merijn Schering <mschering@intermesh.nl>
2018-06-05 16:51:36 +0200

    Wrong substitution

--- docker-go-entrypoint.sh
+++ docker-go-entrypoint.sh
@@ -7,8 +7,8 @@ cp /etc/groupoffice/config.php.tpl /etc/groupoffice/config.php
 
 sed -i 's/{dbHost}/'${MYSQL_HOST}'/' /etc/groupoffice/config.php
 sed -i 's/{dbName}/'${MYSQL_DATABASE}'/' /etc/groupoffice/config.php
-sed -i 's/{dbUser}/'${MYSQL_PASSWORD}'/' /etc/groupoffice/config.php
-sed -i 's/{dbPass}/'${MYSQL_USER}'/' /etc/groupoffice/config.php
+sed -i 's/{dbUser}/'${MYSQL_USER}'/' /etc/groupoffice/config.php
+sed -i 's/{dbPass}/'${MYSQL_PASSWORD}'/' /etc/groupoffice/config.php
 
 #call original entry point
 docker-php-entrypoint "$@"
