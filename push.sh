7b380f50f78ed735812c83322d679fe8e0824055
Merijn Schering <mschering@intermesh.nl>
2023-04-04 10:59:46 +0200

    6.7 and latest

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-docker build --no-cache . -t intermesh/groupoffice:latest -t intermesh/6.7
+docker build --no-cache -t intermesh/groupoffice:latest -t intermesh/6.7 .
 docker login
 docker push intermesh/groupoffice:latest
 docker push intermesh/groupoffice:6.7
