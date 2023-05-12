bc5f69d32c85e2abc9cd84b1567ac26317a17f92
Merijn Schering <mschering@intermesh.nl>
2023-05-12 16:45:54 +0200

    6.7.28

--- push.sh
+++ push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-docker build . -t intermesh/groupoffice:6.6
 docker login
 docker buildx build --push --platform linux/amd64,linux/arm64 -t intermesh/groupoffice:latest -t intermesh/groupoffice:6.7 .
