1f526a3be2653a2a2b9ab8bbfa678e992a14cedf
Merijn Schering <mschering@intermesh.nl>
2023-10-03 15:33:14 +0200

    Tag patch version number too. Fixes issue #15

--- push.sh
+++ push.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
+
+
+CONTENT=`cat Dockerfile`
+REGEX='VERSION=([0-9]+)\.([0-9]+)\.([0-9]+)';
+
+[[ $CONTENT =~ $REGEX ]]
+
+major=${BASH_REMATCH[1]};
+minor=${BASH_REMATCH[2]};
+patch=${BASH_REMATCH[3]};
+
 docker login
-docker buildx build --push --platform linux/amd64,linux/arm64 -t intermesh/groupoffice:6.7 .
+docker buildx build --push --platform linux/amd64,linux/arm64 -t intermesh/groupoffice:$major.$minor -t intermesh/groupoffice $major.$minor.$patch .
