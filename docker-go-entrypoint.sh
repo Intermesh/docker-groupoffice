c505f3ca92f5106adb6c8ad939e75c70430b0e5e
mschering <mschering@intermesh.nl>
2025-04-08 11:02:55 +0200

    chmod not needed

--- docker-go-entrypoint.sh
+++ docker-go-entrypoint.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 set -e
 
-chown -R www-data:www-data /var/lib/groupoffice
+cp /usr/local/share/groupoffice-docker-config.php.tpl /etc/groupoffice/docker-config.php
 
-#if [ ! -f "/etc/groupoffice/config.php" ]; then
-  cp /usr/local/share/groupoffice-docker-config.php.tpl /etc/groupoffice/docker-config.php
+sed -i 's/{dbHost}/'${MYSQL_HOST}'/' /etc/groupoffice/docker-config.php
+sed -i 's/{dbName}/'${MYSQL_DATABASE}'/' /etc/groupoffice/docker-config.php
+sed -i 's/{dbUser}/'${MYSQL_USER}'/' /etc/groupoffice/docker-config.php
+sed -i 's/{dbPass}/'${MYSQL_PASSWORD}'/' /etc/groupoffice/docker-config.php
 
-  sed -i 's/{dbHost}/'${MYSQL_HOST}'/' /etc/groupoffice/docker-config.php
-  sed -i 's/{dbName}/'${MYSQL_DATABASE}'/' /etc/groupoffice/docker-config.php
-  sed -i 's/{dbUser}/'${MYSQL_USER}'/' /etc/groupoffice/docker-config.php
-  sed -i 's/{dbPass}/'${MYSQL_PASSWORD}'/' /etc/groupoffice/docker-config.php
-#fi
 
 #call original entry point
 docker-php-entrypoint "$@"
